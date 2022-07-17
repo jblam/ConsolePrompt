e6d5e48321e6b284ea9d4bb9a231960cdaea24bf
jblam <jeremy.blamey@gmail.com>
2022-07-17 19:33:31 +1000

    Added doc-comments, packaging information

--- JBlam.ConsolePrompt/ConsolePromptInterpolationHandler.cs
+++ JBlam.ConsolePrompt/ConsolePromptInterpolationHandler.cs
@@ -3,6 +3,11 @@ using System.Text;
 
 namespace JBlam.ConsolePrompt;
 
+/// <summary>
+/// Provides a handler used by the language compiler to append interpolated 
+/// strings into <see cref="Out.Write(ConsolePromptInterpolationHandler)"/>
+/// and <see cref="Out.WriteLine(ConsolePromptInterpolationHandler)"/>.
+/// </summary>
 [InterpolatedStringHandler]
 public ref struct ConsolePromptInterpolationHandler
 {
@@ -23,68 +28,100 @@ public ref struct ConsolePromptInterpolationHandler
     readonly StringBuilder b;
     readonly List<(Range, ConsoleColor)> colours;
 
+    /// <summary>
+    /// Creates a handler used to write an interpolated string to the console.
+    /// </summary>
+    /// <param name="literalLength">
+    /// The number of literal characters outside of interpolation expressions 
+    /// in the interpolated string.
+    /// </param>
+    /// <param name="formattedCount">
+    /// The number of interpolation expressions in the interpolated string.
+    /// </param>
     public ConsolePromptInterpolationHandler(int literalLength, int formattedCount)
     {
-        // JB 2022-07-02: literalLength is the _minimum_ length; if formattedCount > 0
-        // we should probably request more?
-        b = new(literalLength);
+        // JB 2022-07-17: following the reference source implementation of StringBuilder's
+        // handler, we're using the estimated "11 chars per formatted item".
+        b = new(literalLength + formattedCount * 11);
         h = new(literalLength, formattedCount, b);
         colours = new(formattedCount);
     }
-    public void AppendLiteral(string s)
+
+    /// <summary>Writes the specified string to the handler.</summary>
+    /// <param name="value">The string to write.</param>
+    public void AppendLiteral(string value)
     {
-        h.AppendLiteral(s);
+        h.AppendLiteral(value);
     }
 
-    public void AppendFormatted<T>(T t)
+    /// <summary>Writes the specified value to the handler.</summary>
+    /// <param name="value">The value to write.</param>
+    public void AppendFormatted<T>(T value)
     {
-        AppendFormattedImpl(t);
-    }
-    public void AppendFormatted<T>(T t, int alignment)
-    {
-        AppendFormattedImpl(t, alignment);
+        AppendFormattedImpl(value);
     }
 
-    public void AppendFormatted<T>(T t, string? format)
+    /// <summary>Writes the specified value to the handler.</summary>
+    /// <param name="value">The value to write.</param>
+    /// <param name="alignment">
+    /// The minimum number of characters that should be written for this value.
+    /// If the value is negative, it indicates left-aligned and the required minimum is the absolute value.
+    /// </param>
+    public void AppendFormatted<T>(T value, int alignment)
+    {
+        AppendFormattedImpl(value, alignment);
+    }
+
+    /// <summary>Writes the specified value to the handler.</summary>
+    /// <param name="value">The value to write.</param>
+    /// <param name="format">The format string.</param>
+    public void AppendFormatted<T>(T value, string? format)
     {
         if (ColourFormat.TryParse(format, out var colour, out var rest))
         {
-            var range = AppendFormattedImpl(t, rest.ToString());
+            var range = AppendFormattedImpl(value, rest.ToString());
             colours.Add((range, colour));
         }
         else
         {
-            AppendFormattedImpl(t, format);
+            AppendFormattedImpl(value, format);
         }
     }
-    public void AppendFormatted<T>(T t, int alignment, string? format)
+    /// <summary>Writes the specified value to the handler.</summary>
+    /// <param name="value">The value to write.</param>
+    /// <param name="format">The format string.</param>
+    /// <param name="alignment">
+    /// The minimum number of characters that should be written for this value.
+    /// If the value is negative, it indicates left-aligned and the required minimum is the absolute value.
+    /// </param>
+    public void AppendFormatted<T>(T value, int alignment, string? format)
     {
         if (ColourFormat.TryParse(format, out var colour, out var rest))
         {
-            var range = AppendFormattedImpl(t, alignment, rest.ToString());
+            var range = AppendFormattedImpl(value, alignment, rest.ToString());
             colours.Add((range, colour));
         }
         else
         {
-            AppendFormattedImpl(t, alignment, format);
+            AppendFormattedImpl(value, alignment, format);
         }
     }
-    Range AppendFormattedImpl<T>(T t, string? format)
+    Range AppendFormattedImpl<T>(T value, string? format)
     {
         var previousLength = b.Length;
-        h.AppendFormatted(t, format);
+        h.AppendFormatted(value, format);
         return previousLength..b.Length;
     }
-    Range AppendFormattedImpl<T>(T t)
+    Range AppendFormattedImpl<T>(T value)
     {
         var previousLength = b.Length;
-        h.AppendFormatted(t);
+        h.AppendFormatted(value);
         return previousLength..b.Length;
     }
-    Range AppendFormattedImpl<T>(T t, int alignment)
+    Range AppendFormattedImpl<T>(T value, int alignment)
     {
         var previousLength = b.Length;
-        h.AppendFormatted(t, alignment);
+        h.AppendFormatted(value, alignment);
         return previousLength..b.Length;
     }
     Range AppendFormattedImpl<T>(T t, int alignment, string? format)
@@ -94,6 +131,13 @@ public ref struct ConsolePromptInterpolationHandler
         return previousLength..b.Length;
     }
 
+    /// <summary>
+    /// Gets a list of the ranges of the interpolated string for which a
+    /// <see cref="ConsoleColor"/> value was specified.
+    /// </summary>
     public IReadOnlyList<(Range, ConsoleColor)> Colours => colours;
+    /// <summary>
+    /// Gets the value of the formatted string.
+    /// </summary>
     public string GetFormatted() => b.ToString();
 }
--- JBlam.ConsolePrompt/Out.cs
+++ JBlam.ConsolePrompt/Out.cs
@@ -1,18 +1,38 @@
 ﻿namespace JBlam.ConsolePrompt;
+
+/// <summary>
+/// Provides equivalent methods to <see cref="Console.Write(string?)"/>
+/// and <see cref="Console.WriteLine(string)"/> which support colour in
+/// the formatting part of an interpolated string.
+/// </summary>
 public static class Out
 {
+    /// <summary>
+    /// Writes the specified interpolated string, followed by the current line terminator,
+    /// to the standard output stream.
+    /// </summary>
+    /// <param name="handler">The interpolated string to write.</param>
+    /// <remarks>
+    /// Colours are specified by the string name of a member of <see cref="ConsoleColor"/>;
+    /// the colour may be given alone (<c>Out.WriteLine($"{value:Green}")</c>),
+    /// or as the prefix to another format string (<c>Out.WriteLine("$Value: {value:Green,X2}");</c>).
+    /// </remarks>
     public static void WriteLine(ConsolePromptInterpolationHandler handler) 
     {
         Out.Write(handler);
         Console.WriteLine();
     }
+    /// <summary>
+    /// Writes the specified interpolated string to the standard output stream.
+    /// </summary>
+    /// <param name="handler">The interpolated string to write.</param>
+    /// <remarks>
+    /// Colours are specified by the string name of a member of <see cref="ConsoleColor"/>;
+    /// the colour may be given alone (<c>Out.Write($"{value:Green}")</c>),
+    /// or as the prefix to another format string (<c>Out.Write("$Value: {value:Green,X2}");</c>).
+    /// </remarks>
     public static void Write(ConsolePromptInterpolationHandler handler)
     {
-        // JB 2022-07-02 perf considerations:
-        // - Console.Write does not take a (ReadOnly)Span<char>
-        // - it _does_ take an old-skool (char[], int, int)
-        // Also worth considering allocs from the stringbuilder;
-        // we could enumerate chunks instead.
         var result = handler.GetFormatted();
         Index position = 0;
         foreach (var (range, colour) in handler.Colours)
